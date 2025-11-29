d6ff8ab697f468eef8f0b034b08df359d6183cb1
unknown <nagilapereira8896@digitalcollege.internal>
2025-11-29 11:17:42 -0300

    inicio react router

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css';
+import AppRoutes from '../routes.jsx';
+import {Link} from 'react-router-dom'
+
+const App = () => {
+
+  return (
+    <>
+      <header>
+        <Link to='/'>Logo</Link>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/products'>Products</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <AppRoutes />
+    </>
+  );
+}
+
+export default App;
